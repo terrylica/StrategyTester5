248b0f06d380fd169f97f455fb779f00becf976a
Omega Joctan <omegajoctan@gmail.com>
2026-02-01 08:42:48 +0300

    broker_data importers, exporters -> code improvement

--- strategytester5/mt5/broker_data.py
+++ strategytester5/mt5/broker_data.py
@@ -14,7 +14,7 @@ class Importers:
         if not os.path.exists(self.broker_path):
             err = "Broker path does not exist"
             self.logger.error(err)
-            raise RuntimeError(err)
+            # raise RuntimeError(err)
 
     def account_info(self) -> AccountInfo:
 
@@ -26,15 +26,18 @@ class Importers:
         try:
             with open(file) as json_file:
                 data = json.load(json_file)
+
+            account_info = AccountInfo(**data["account_info"])
+
         except Exception as e:
             self.logger.error(e)
             return None
 
-        return AccountInfo(**data["account_info"])
+        return account_info
 
     def all_symbol_info(self) -> tuple:
 
-        file = os.path.join(self.broker_path, ACCOUNT_INFO_FILE)
+        file = os.path.join(self.broker_path, SYMBOL_INFO_FILE)
         if not os.path.exists(file):
             self.logger.error(f"Failed to import account info, {file} not found")
             return None
@@ -42,20 +45,25 @@ class Importers:
         try:
             with open(file) as json_file:
                 data = json.load(json_file)
+
+            all_symbol_info = []
+
+            for s in data["all_symbols_info"]:
+                all_symbol_info.append(SymbolInfo(**s))
         except Exception as e:
-            self.logger.error(e)
-
-        all_symbol_info = []
-
-        for s in data["all_symbols_info"]:
-            all_symbol_info.append(SymbolInfo(**s))
+            self.logger.error(f"Failed to read a file {file} error = {e}")
+            return None
 
         return tuple(all_symbol_info)
 
 class Exporters:
-    def __init__(self, broker_path: str, logger: logging.Logger):
+    def __init__(self, broker_path: str, logger: logging.Logger, file_not_exists: bool = True):
+        """
+        file_not_exists (bool): When set to True, exports information to non-existing files, if a file exists it will be ignored
+        """
         self.broker_path = broker_path
         self.logger = logger
+        self.file_not_exists = file_not_exists
 
         try:
             os.makedirs(self.broker_path, exist_ok=True)
@@ -66,6 +74,11 @@ class Exporters:
     def account_info(self, ac_info: AccountInfo | dict) -> bool:
 
         file = os.path.join(self.broker_path, ACCOUNT_INFO_FILE)
+
+        if self.file_not_exists:  # file must not exist mode
+            if os.path.exists(file):
+                self.logger.debug(f"Failed to export account info, {file} is found, to explicitly save the file set 'file_not_exist=False'")
+                return False
 
         try:
             payload = {
@@ -82,6 +95,11 @@ class Exporters:
     def all_symbol_info(self, all_symbol_info: tuple | list) -> bool:
 
         file = os.path.join(self.broker_path, SYMBOL_INFO_FILE)
+
+        if self.file_not_exists: # file must not exist mode
+            if os.path.exists(file):
+                self.logger.debug(f"Failed to export symbol info, {file} is found, to explicitly save the file set 'file_not_exist=False'")
+                return False
 
         try:
             exported = []
